45732074711b787e15fabd873f72b7d8c5da7e38
masi <masi.woermann@gmail.com>
2016-09-15 22:18:17 +0100

    State

--- views/index.jsx
+++ views/index.jsx
@@ -29,13 +29,25 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {checked: false}
+  }
+  
   render() {
     return (
       <tr>
+	<td style={{border: "1px solid black"}}>
+	  <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+	</td>
 	<td style={{border: "1px solid black"}}>{this.props.title}</td>
 	<td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
     )
+  }
+  
+  handleChange(event) {
+    this.setState({checked: false})
   }
 }
 Todo.propTypes = {
