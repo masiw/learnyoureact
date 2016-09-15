cc442589380ffe61925ddce5ba55375e2ba92aa9
masi <masi.woermann@gmail.com>
2016-09-15 22:25:02 +0100

    CSS

--- views/index.jsx
+++ views/index.jsx
@@ -16,7 +16,7 @@ class TodoList extends React.Component {
   render() {
     return (
       <div className="todoList">
-	<table style={{border: "2px solid black"}}>
+	<table style={style.table}>
 	  <tbody>
 	    <Todo title="Shopping">Milk</Todo>
 	    <Todo title="Hair cut">13:00</Todo>
@@ -37,11 +37,11 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-	<td style={{border: "1px solid black"}}>
+	<td style={style.tableContent}>
 	  <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
 	</td>
-	<td style={{border: "1px solid black"}}>{this.props.title}</td>
-	<td style={{border: "1px solid black"}}>{this.props.children}</td>
+	<td style={style.tableContent}>{this.props.title}</td>
+	<td style={style.tableContent}>{this.props.children}</td>
       </tr>
     )
   }
@@ -61,5 +61,14 @@ class TodoForm extends React.Component {
 	I am a TodoForm.
       </div>
     )
+  }
+}
+
+let style = {
+  table: {
+    border: "2px solid black"
+  },
+  tableContent: {
+    border: "1px solid black"
   }
 }
